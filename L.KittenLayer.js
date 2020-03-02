ff5c7c00dde7667d8618ec4038e1b0c8fe032c3c
Emmanuel <E.Chamilakis@let.ru.nl>
2020-03-02 12:46:36 +0100

    Added some extra examples

---
+++ L.KittenLayer.js
@@ -0,0 +1,9 @@
+L.TileLayer.Kitten = L.TileLayer.extend({
+    getTileUrl: function(coords) {
+        var i = Math.ceil( Math.random() * 4 );
+        return "https://placekitten.com/256/256?image=" + i;
+    },
+    getAttribution: function() {
+        return "<a href='https://placekitten.com/attribution.html'>PlaceKitten</a>"
+    }
+});
